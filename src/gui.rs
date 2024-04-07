f54124b8730974a5ca7c763e20256123397fd2f1
Danko Aleksejevs <danko@very.lv>
2024-04-07 10:30:23 +0300

    Batch drawing into "layers"

--- src/gui.rs
+++ src/gui.rs
@@ -2,14 +2,14 @@ use std::f32::consts::{PI, TAU};
 use std::num::NonZeroU32;
 use std::sync::{Arc, RwLock};
 
-use femtovg::{Canvas, Color, Paint, Path, Renderer, Solidity};
 use femtovg::renderer::OpenGl;
+use femtovg::{Canvas, Color, Paint, Path, Renderer, Solidity};
+use glutin::display::Display;
+use glutin::surface::{Surface, SurfaceAttributesBuilder, WindowSurface};
 use glutin::{
 	config::ConfigTemplateBuilder, context::ContextAttributesBuilder, context::PossiblyCurrentContext,
 	display::GetGlDisplay, prelude::*,
 };
-use glutin::display::Display;
-use glutin::surface::{Surface, SurfaceAttributesBuilder, WindowSurface};
 use glutin_winit::DisplayBuilder;
 use raw_window_handle::HasRawWindowHandle;
 use winit::event::{Event, WindowEvent};
@@ -50,6 +50,7 @@ pub fn init_gui() -> (
 		.unwrap();
 
 	let window = window.unwrap();
+	// window.set_cursor_hittest(false).unwrap();
 
 	let gl_display = gl_config.display();
 
@@ -70,6 +71,7 @@ pub fn init_gui() -> (
 
 	(event_loop, current_context, gl_display, window, surface)
 }
+
 pub fn gui_loop(
 	event_loop: EventLoop<UIEvent>,
 	context: PossiblyCurrentContext,
@@ -81,6 +83,8 @@ pub fn gui_loop(
 		.expect("Cannot create renderer");
 
 	let mut canvas = Canvas::new(renderer).expect("Cannot create canvas");
+	
+	// let fg_fb = context.create_framebuffer();
 
 	canvas.add_font("assets/DejaVuSansMono.ttf").unwrap();
 
@@ -96,6 +100,7 @@ pub fn gui_loop(
 						if let Some((config, state)) = &state_refs {
 							render_gui(&context, &surface, &window, &mut canvas, &state, &config);
 						}
+						window.focus_window();
 					}
 					_ => {}
 				}
@@ -116,6 +121,29 @@ pub fn gui_loop(
 	Ok(())
 }
 
+#[derive(Debug, Clone)]
+enum CanvasDrawable {
+	Path(Path),
+	Text(String)
+}
+
+struct Draw<'a> {
+	what: CanvasDrawable,
+	fill: Option<&'a Paint>,
+	stroke: Option<&'a Paint>,
+	x: f32,
+	y: f32,
+}
+
+impl <'a> Draw<'a> {
+	fn text(x: f32, y: f32, text: String, fill: Option<&'a Paint>, stroke: Option<&'a Paint>) -> Draw<'a> {
+		Self { what: CanvasDrawable::Text(text), fill, stroke, x, y }
+	}
+	fn path(path: Path, fill: Option<&'a Paint>, stroke: Option<&'a Paint>) -> Draw<'a> {
+		Self { what: CanvasDrawable::Path(path), fill, stroke, x: 0., y: 0. }
+	}
+}
+
 fn render_gui<T: Renderer>(
 	context: &PossiblyCurrentContext,
 	surface: &Surface<WindowSurface>,
@@ -124,6 +152,7 @@ fn render_gui<T: Renderer>(
 	state: &Arc<RwLock<State>>,
 	config: &Arc<CachedConfig>,
 ) {
+	
 	let size = window.inner_size();
 	canvas.set_size(size.width, size.height, window.scale_factor() as f32);
 	let scale = size.width.min(size.height) as f32 * window.scale_factor() as f32 / 1000.0;
@@ -132,23 +161,40 @@ fn render_gui<T: Renderer>(
 
 	let mut bg = Path::new();
 	bg.rounded_rect(0., 0., canvas.width() as f32, canvas.height() as f32, 100. * scale);
-	canvas.fill_path(&bg, &Paint::color(Color::rgbaf(0.9, 0.9, 0.9, 0.5)));
+	canvas.fill_path(&bg, &Paint::color(Color::rgbaf(0.9, 0.9, 0.9, 0.3)));
 
 	let btn_fill = Paint::color(Color::rgbaf(1., 1., 1., 1.));
-	let btn_stroke = Paint::color(Color::rgbaf(0., 0., 0., 1.));
+	let btn_stroke = Paint::color(Color::rgbaf(0., 0., 0., 1.)).with_line_width(0.8);
 	let active_fill = Paint::color(Color::rgbaf(0.7, 0.5, 1.0, 1.));
 	let in_combo_fill = Paint::color(Color::rgbaf(0.7, 0.5, 1.0, 0.3));
+	let text_bg_fill = Paint::color(Color::rgbaf(1., 1., 0., 1.));
 	let text_fill = Paint::color(Color::rgbaf(0., 0., 0., 1.));
+	let text_padding = 8.0 * scale;
+	
+	let mut backgrounds: Vec<Draw> = Vec::new();
+	let mut shapes: Vec<Draw> = Vec::new();
+	let mut highlights: Vec<Draw> = Vec::new();
+	let mut labels: Vec<Draw> = Vec::new();
 
-	let draw_label = |canvas: &mut Canvas<T>, s: &str, x, y, x_off| {
+	let mut draw_label = |canvas: &Canvas<T>, s: &str, x, y, x_off| {
 		let m = canvas.measure_text(x, y, s, &text_fill).unwrap();
 		let dy = y * scale + m.height() * 0.5;
 		let dx = (x + x_off) * scale - if x_off >= 0. { 0. } else { m.width() };
-		println!("{:.1} {:.1}", dx, dy);
-		canvas.fill_text(dx, dy, s, &text_fill).unwrap();
+		// println!("{:.1} {:.1}", dx, dy);
+
+		let mut bg = Path::new();
+		bg.rounded_rect(
+			dx - text_padding,
+			y * scale - m.height() * 0.5 - text_padding,
+			m.width() + text_padding * 2.,
+			m.height() + text_padding * 2.,
+			text_padding,
+		);
+		backgrounds.push(Draw::path(bg, Some(&text_bg_fill), None));
+		labels.push(Draw::text(dx, dy, String::from(s), Some(&text_fill), None));
 	};
 
-	let draw_handler_label = |canvas: &mut Canvas<T>, hdl: &ButtonHandler, x, y, x_off| {
+	let mut draw_handler_label = |canvas: &Canvas<T>, hdl: &ButtonHandler, x, y, x_off| {
 		if let Some(s) = &hdl.label {
 			draw_label(canvas, s, x, y, x_off);
 		} else if let Some(m) = &hdl.map {
@@ -175,8 +221,7 @@ fn render_gui<T: Renderer>(
 				TAU,
 				Solidity::Solid,
 			);
-			canvas.fill_path(&p, &btn_fill);
-			canvas.stroke_path(&p, &btn_stroke);
+			shapes.push(Draw::path(p, Some(&btn_fill), Some(&btn_stroke)));
 
 			let bs = &state.button_states[c.button as usize];
 			if bs.down {
@@ -189,14 +234,12 @@ fn render_gui<T: Renderer>(
 					TAU,
 					Solidity::Solid,
 				);
-				canvas.fill_path(
-					&p,
-					if bs.in_combo.is_some() {
-						&in_combo_fill
-					} else {
-						&active_fill
-					},
-				);
+				let fill = if bs.in_combo.is_some() {
+					&in_combo_fill
+				} else {
+					&active_fill
+				};
+				shapes.push(Draw::path(p, Some(&fill), None));
 			}
 
 			let hdl = state.find_button_handler(config, &btn);
@@ -215,8 +258,7 @@ fn render_gui<T: Renderer>(
 				TAU,
 				Solidity::Solid,
 			);
-			canvas.fill_path(&p, &btn_fill);
-			canvas.stroke_path(&p, &btn_stroke);
+			shapes.push(Draw::path(p, Some(&btn_fill), Some(&btn_stroke)));
 
 			if state.combo_states[c_idx] {
 				let mut p = Path::new();
@@ -228,7 +270,7 @@ fn render_gui<T: Renderer>(
 					TAU,
 					Solidity::Solid,
 				);
-				canvas.fill_path(&p, &active_fill);
+				shapes.push(Draw::path(p, Some(&active_fill), None));
 			}
 
 			let hdl = state.find_combo_handler(config, c_idx);
@@ -254,8 +296,7 @@ fn render_gui<T: Renderer>(
 
 			let mut p = Path::new();
 			p.arc(x, y, r, 0., TAU, Solidity::Solid);
-			canvas.fill_path(&p, &btn_fill);
-			canvas.stroke_path(&p, &btn_stroke);
+			shapes.push(Draw::path(p, Some(&btn_fill), Some(&btn_stroke)));
 
 			let hdl = state.find_stick_handler(config, s_idx);
 			if let Some((_, hdl)) = hdl {
@@ -282,34 +323,62 @@ fn render_gui<T: Renderer>(
 							sec.from_degrees.to_radians() as f32 + PI,
 							Solidity::Solid,
 						);
-						if ss.sector.is_some_and(|a| a == sec_idx) {
-							canvas.fill_path(&p, &active_fill);
-						}
-						canvas.stroke_path(&p, &btn_stroke);
+						shapes.push(Draw::path(
+							p,
+							if ss.sector.is_some_and(|a| a == sec_idx) { Some(&active_fill) } else { None },
+							Some(&btn_stroke),
+						));
 					}
 				}
 			}
-			
+
 			let mut p = Path::new();
 			p.arc(x, y, c.center.border_radius * scale, 0., TAU, Solidity::Solid);
-			canvas.fill_path(&p, &btn_fill);
-			canvas.stroke_path(&p, &btn_stroke);
+			shapes.push(Draw::path(p, Some(&btn_fill), Some(&btn_stroke)));
 
 			let px = x - ss.value as f32 * r * ss.degrees.to_radians().cos() as f32;
 			let py = y - ss.value as f32 * r * ss.degrees.to_radians().sin() as f32;
 			let mut p = Path::new();
 			p.arc(px, py, c.point.border_radius * scale, 0., TAU, Solidity::Solid);
-			canvas.fill_path(&p, &btn_fill);
-			canvas.stroke_path(&p, &btn_stroke);
+			shapes.push(Draw::path(p, Some(&btn_fill), Some(&btn_stroke)));
 
 			if ss.sector.is_some() {
 				let mut p = Path::new();
 				p.arc(px, py, c.point.fill_radius * scale, 0., TAU, Solidity::Solid);
-				canvas.fill_path(&p, &active_fill);
+				shapes.push(Draw::path(p, Some(&active_fill), None));
 			}
 		}
 	}
+	
+	let mut write_commands = |list: Vec<Draw>| {
+		for cmd in list {
+			match cmd.what {
+				CanvasDrawable::Path(path) => {
+					if let Some(paint) = cmd.fill {
+						canvas.fill_path(&path, &paint);
+					}
+					if let Some(paint) = cmd.stroke {
+						canvas.stroke_path(&path, &paint);
+					}
+				}
+				CanvasDrawable::Text(text) => {
+					if let Some(paint) = cmd.fill {
+						canvas.fill_text(cmd.x, cmd.y, &text, &paint).unwrap();
+					}
+					if let Some(paint) = cmd.stroke {
+						canvas.stroke_text(cmd.x, cmd.y, &text, &paint).unwrap();
+					}
+				}
+			}
+		}
+	};
+	
+	write_commands(backgrounds);
+	write_commands(shapes);
+	write_commands(highlights);
+	write_commands(labels);
 
 	canvas.flush();
 	surface.swap_buffers(context).expect("Could not swap buffers");
+	
 }
